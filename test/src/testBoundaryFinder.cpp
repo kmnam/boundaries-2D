a92e12362df74c3c35c9de7bf3ab74fa774f78f1
kmnam <kmnam@g.harvard.edu>
2019-11-11 18:47:01 -0500

    Updated to check for and remove duplicate points

--- test/src/testBoundaryFinder.cpp
+++ test/src/testBoundaryFinder.cpp
@@ -11,13 +11,13 @@
  * Authors:
  *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
  * Last updated:
- *     11/10/2019
+ *     11/11/2019
  */
 using namespace Eigen;
+boost::random::uniform_int_distribution<> dist(0, 1);
 
 int coin_toss(boost::random::mt19937& rng)
 {
-    boost::random::uniform_int_distribution<> dist(0, 1);
     return dist(rng);
 }
 
@@ -41,22 +41,29 @@ Matrix<T, Dynamic, 1> add_delta(const Ref<const Matrix<T, Dynamic, 1> >& x, boos
                                 LinearConstraints* constraints)
 {
     Matrix<T, Dynamic, 1> y(x.size());
-    T delta = 0.01;
+    T delta = 0.1;
     for (unsigned i = 0; i < x.size(); ++i)
     {
         int toss = coin_toss(rng);
-        if (!toss) y(i) = x(i) + delta;
-        else       y(i) = x(i) - delta;
+        y(i) = (!toss ? x(i) + delta : x(i) - delta);
     }
     return constraints->nearestL2(y.template cast<double>()).template cast<T>();
 }
 
-int main()
+// -------------------------------------------------------- //
+//                 BOUNDARY FINDER FUNCTIONS                //
+// -------------------------------------------------------- //
+void testBoundaryFinderProject()
 {
+    /*
+     * Example 1A: projection onto the first two coordinates out of six,
+     * constrained to the 6-D cube of side length 2
+     */
     const unsigned seed = 1234567890;
+    const unsigned max_iter = 20;    // Maximum of 20 iterations 
+    const double tol = 1e-5;         // Tolerance for convergence
+    const unsigned n_init = 100;     // Initial number of parameter points
 
-    // Example 1: projection onto the first two coordinates out of six,
-    // constrained to the 6-D cube of side length 2
     MatrixXd A(12, 6);
     VectorXd b(12);
     A <<  1.0,  0.0,  0.0,  0.0,  0.0,  0.0,
@@ -72,13 +79,49 @@ int main()
           0.0,  0.0,  0.0,  0.0, -1.0,  0.0,
           0.0,  0.0,  0.0,  0.0,  0.0, -1.0;
     b << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0;
-    unsigned max_iter = 20;    // Maximum of 20 iterations 
-    double tol = 1e-5;         // Tolerance for convergence
-    unsigned n_init = 100;     // Initial number of parameter points
-    BoundaryFinder finder(6, tol, max_iter, 1234567890, A, b);
+    BoundaryFinder finder(6, tol, max_iter, seed, A, b);
     MatrixXd params = MatrixXd::Ones(n_init, 6) + MatrixXd::Random(n_init, 6);
     std::function<VectorXvar(const Ref<const VectorXvar>&)> func = project<var>;
     std::function<VectorXvar(const Ref<const VectorXvar>&, boost::random::mt19937&, LinearConstraints*)> mutate = add_delta<var>; 
     finder.run(func, mutate, params, false, true, "project");
+}
+
+void testBoundaryFinderProjectSimplified()
+{
+    /*
+     * Example 1B: projection onto the first two coordinates out of six,
+     * constrained to the 6-D cube of side length 2, with simplification
+     */
+    const unsigned seed = 1234567890;
+    const unsigned max_iter = 20;    // Maximum of 20 iterations 
+    const double tol = 1e-5;         // Tolerance for convergence
+    const unsigned n_init = 100;     // Initial number of parameter points
+
+    MatrixXd A(12, 6);
+    VectorXd b(12);
+    A <<  1.0,  0.0,  0.0,  0.0,  0.0,  0.0,
+          0.0,  1.0,  0.0,  0.0,  0.0,  0.0,
+          0.0,  0.0,  1.0,  0.0,  0.0,  0.0,
+          0.0,  0.0,  0.0,  1.0,  0.0,  0.0,
+          0.0,  0.0,  0.0,  0.0,  1.0,  0.0,
+          0.0,  0.0,  0.0,  0.0,  0.0,  1.0,
+         -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,
+          0.0, -1.0,  0.0,  0.0,  0.0,  0.0,
+          0.0,  0.0, -1.0,  0.0,  0.0,  0.0,
+          0.0,  0.0,  0.0, -1.0,  0.0,  0.0,
+          0.0,  0.0,  0.0,  0.0, -1.0,  0.0,
+          0.0,  0.0,  0.0,  0.0,  0.0, -1.0;
+    b << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0;
+    BoundaryFinder finder(6, tol, max_iter, seed, A, b);
+    MatrixXd params = MatrixXd::Ones(n_init, 6) + MatrixXd::Random(n_init, 6);
+    std::function<VectorXvar(const Ref<const VectorXvar>&)> func = project<var>;
+    std::function<VectorXvar(const Ref<const VectorXvar>&, boost::random::mt19937&, LinearConstraints*)> mutate = add_delta<var>; 
+    finder.run(func, mutate, params, true, true, "project-simplified");
+}
+
+int main()
+{
+    testBoundaryFinderProject();
+    testBoundaryFinderProjectSimplified();
     return 0;
 }
