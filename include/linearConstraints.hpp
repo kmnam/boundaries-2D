af186cb7bbba250e8430af9d77a470b53cbb798e
kmnam <kmnam@g.harvard.edu>
2019-11-11 20:25:58 -0500

    Removed functions with std::vector arguments and implemented active()

--- include/linearConstraints.hpp
+++ include/linearConstraints.hpp
@@ -123,33 +123,12 @@ class LinearConstraints
             return this->b;
         }
 
-        bool check(const std::vector<double>& x)
-        {
-            /*
-             * Return true if no constraints were satisfied or, otherwise, 
-             * if this-> A * x >= this->b.
-             */
-            if (this->N == 0) return true;
-            if (x.size() != this->D)
-            {
-                std::stringstream ss;
-                ss << "Dimensions of A and x do not match: ("
-                   << this->A.rows() << "," << this->A.cols()
-                   << ") vs. " << x.size() << std::endl;
-                throw std::invalid_argument(ss.str());
-            }
-            VectorXd v(x.size());
-            for (unsigned i = 0; i < x.size(); ++i) v(i) = x[i];
-            return ((this->A * v).array() >= (this->b).array()).all();
-        }
-
         bool check(const Ref<const VectorXd>& x)
         {
             /* 
              * Return true if no constraints were satisfied or, otherwise,
              * if this->A * x >= this->b.
              */
-            if (this->N == 0) return true;
             if (x.size() != this->D)
             {
                 std::stringstream ss;
@@ -161,39 +140,22 @@ class LinearConstraints
             return ((this->A * x).array() >= (this->b).array()).all();
         }
 
-        std::vector<double> nearestL2(const std::vector<double>& x)
+        Matrix<bool, Dynamic, 1> active(const Ref<const VectorXd>& x)
         {
             /*
-             * Return the nearest point to x, with respect to L2 distance,
-             * that satisfies the given constraints.
+             * Return a boolean vector indicating which constraints 
+             * are active (i.e., which constraints are satisfied as 
+             * equalities).
              */
-            // First check that x itself satisfies the constraints
-            if (this->check(x))
+            if (x.size() != this->D)
             {
-                std::vector<double> y(x);
-                return y;
+                std::stringstream ss;
+                ss << "Dimensions of A and x do not match: ("
+                   << this->A.rows() << "," << this->A.cols()
+                   << ") vs. " << x.size() << std::endl;
+                throw std::invalid_argument(ss.str());
             }
-
-            // Otherwise, solve the quadratic program for the nearest point to x
-            for (unsigned i = 0; i < this->D; ++i)
-                this->nearest_L2->set_c(i, -2.0 * x[i]);
-            Solution solution = CGAL::solve_quadratic_program(*this->nearest_L2, ET()); 
-            if (solution.is_infeasible())
-                throw std::runtime_error("Quadratic program is infeasible");
-            else if (solution.is_unbounded())
-                throw std::runtime_error("Quadratic program is unbounded");
-            else if (!solution.is_optimal())
-                throw std::runtime_error("Failed to compute optimal solution");
-
-            // Collect the values of the solution into a VectorXd
-            std::vector<double> y;
-            unsigned i = 0;
-            for (auto it = solution.variable_values_begin(); it != solution.variable_values_end(); ++it)
-            {
-                y.push_back(CGAL::to_double(*it));
-                i++;
-            }
-            return y;
+            return ((this->A * x).array() == (this->b).array());
         }
 
         VectorXd nearestL2(const Ref<const VectorXd>& x)
