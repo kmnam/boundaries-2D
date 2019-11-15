23f81b14b35ad52bddc7d4e74494a0801e58974e
Kee Myoung Nam <kmnam@g.harvard.edu>
2019-11-14 22:38:35 -0500

    Implemented gradient()

--- include/duals/eigen.hpp
+++ include/duals/eigen.hpp
@@ -1,7 +1,17 @@
 #ifndef DUAL_NUMBER_EIGEN_SUPPORT_HPP
 #define DUAL_NUMBER_EIGEN_SUPPORT_HPP
+#include <functional>
 #include <Eigen/Dense>
 #include "duals.hpp"
+
+/*
+ * Eigen support header file for the Duals::DualNumber class.
+ *
+ * Authors:
+ *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
+ * Last updated:
+ *     11/14/2019
+ */
 
 namespace Eigen {
 
@@ -24,6 +34,40 @@ struct NumTraits<Duals::DualNumber> : NumTraits<double>
     };
 };
 
+// Convenience typedefs for DualNumber vectors and matrices
+typedef Matrix<Duals::DualNumber, Dynamic, Dynamic> MatrixXDual;
+typedef Matrix<Duals::DualNumber, Dynamic, 1>       VectorXDual;
+
 }   // namespace Eigen
+
+namespace Duals {
+
+using namespace Eigen;
+
+VectorXd gradient(std::function<DualNumber(const Ref<const VectorXDual>&)> func,
+                  const Ref<const VectorXDual>& x, DualNumber& v)
+{
+    /*
+     * Compute the gradient of the given multivariable function at the
+     * given vector of values.  
+     */
+    v = func(x);    // Evaluate func at x and store result in v
+
+    unsigned dim = x.size();
+    VectorXDual xi(dim);
+    VectorXd grad(dim);
+    for (unsigned i = 0; i < dim; ++i)
+    {
+        // Set the nilpotent part to 1 for the i-th coordinate and 0 elsewhere
+        for (unsigned j = 0; j < dim; ++j) xi(j) = x(j).x();
+        xi(i) = DualNumber(x(i).x(), 1.0);
+
+        // Compute the partial derivative w.r.t. the i-th coordinate
+        grad(i) = func(xi).d();
+    }
+    return grad; 
+}
+
+}   // namespace Duals
 
 #endif
