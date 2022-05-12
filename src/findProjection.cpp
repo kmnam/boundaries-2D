2a80e2c68aedbefbb4d75732a78139a2c8f72506
Kee Myoung Nam <kmnam@g.harvard.edu>
2022-05-12 18:15:15 +0900

    Updated to reflect changes in BoundaryFinder mutation protocol

--- src/findProjection.cpp
+++ src/findProjection.cpp
@@ -16,31 +16,53 @@
  *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
  * 
  * **Last updated:**
- *     5/9/2022
+ *     5/12/2022
  */
 using boost::multiprecision::mpq_rational;
+
+/**
+ * Get the maximum distance between any pair of vertices in the given matrix.
+ *
+ * @param vertices Matrix of vertex coordinates.
+ * @returns        Maximum distance between any pair of vertices. 
+ */
+template <typename T>
+T getMaxDist(const Ref<const Matrix<T, Dynamic, Dynamic> >& vertices)
+{
+    T maxdist = 0;
+    for (int i = 0; i < vertices.rows() - 1; ++i)
+    {
+        for (int j = i + 1; j < vertices.rows(); ++j)
+        {
+            T dist = (vertices.row(i) - vertices.row(j)).norm(); 
+            if (maxdist < dist)
+                maxdist = dist; 
+        }
+    }
+    
+    return maxdist; 
+}
 
 int main(int argc, char** argv)
 {
     boost::random::mt19937 rng(1234567890);
-    std::uniform_int_distribution<> fair_bernoulli_dist(0, 1);
-    auto coin_toss = [&fair_bernoulli_dist](boost::random::mt19937& rng)
-    {
-        return fair_bernoulli_dist(rng);
-    };
+    boost::random::uniform_int_distribution<> dist(1, 10000000); 
 
     // Check and parse input arguments
     std::string infilename, outprefix;
-    bool simplify = false;
-    int max_edges = 0; 
-    if (argc == 2)    // Only acceptable call here is "./findProjection --help"
+    int max_edges = 30;        // Maximum number of 30 edges by default
+    int max_step_iter = 10;    // Maximum number of 10 mutation iterations by default
+    int max_pull_iter = 10;    // Maximum number of 10 pulling iterations by default 
+    if (argc == 2)             // Only acceptable call here is "./findProjection --help"
     {
         std::string option(argv[1]);
         if (option == "-h" || option == "--help")
         {
             std::stringstream ss;
-            ss << "\nCommand:\n\t./findProjection [-s NUM_EDGES] INPUT OUTPUT\n\n";
+            ss << "\nCommand:\n\t./findProjection [-s NUM_EDGES] [-m MAX_MUTATION_ITER] [-p MAX_PULL_ITER] INPUT OUTPUT\n\n";
             ss << "\t-s (optional): Simplify alpha shape to contain at most the given number of edges\n";
+            ss << "\t-m (optional): Maximum number of mutation iterations\n";
+            ss << "\t-p (optional): Maximum number of pulling iterations\n";
             ss << "\tINPUT: Input file\n";
             ss << "\tOUTPUT: Output file prefix\n\n";
             std::cerr << ss.str();
@@ -60,44 +82,75 @@ int main(int argc, char** argv)
         infilename = argv[1];
         outprefix = argv[2];
     }
-    else if (argc == 4)    // No acceptable call here
+    else if (argc == 5 || argc == 7 || argc == 9)    // Optional arguments specified
     {
-        std::stringstream ss;
-        ss << "Unrecognized input command\n\n";
-        ss << "Help:\n\t./findProjection --help\n\n";
-        std::cerr << ss.str();
-        return -1;
-    }
-    else if (argc == 5)    // Only acceptable call here is "./findProjection -s NUM_EDGES INPUT OUTPUT"
-    {
-        std::string option(argv[1]);
-        if (option == "-s")
+        std::vector<std::string> options;
+        std::vector<std::string> tokens;
+        std::vector<int> args;
+        for (int i = 1; i < argc - 3; i += 2)
         {
-            simplify = true;
-            std::string token(argv[2]); 
+            options.push_back(argv[i]);
+            tokens.push_back(argv[i + 1]); 
+        } 
+
+        // Disallow re-specification of the same argument
+        for (int i = 0; i < options.size() - 1; ++i)
+        {
+            for (int j = i + 1; j < options.size(); ++j)
+            {
+                if (options[i] == options[j])
+                {
+                    std::stringstream ss; 
+                    ss << "Optional argument specified multiple times: " << options[i] << "\n\n"; 
+                    ss << "Help:\n\t./findProjection --help\n\n";
+                    std::cerr << ss.str();
+                    return -1;  
+                }
+            }
+        } 
+
+        // Interpret each token 
+        for (std::string token : tokens)
+        {
             if (std::regex_match(token, std::regex("((\\+|-)?[[:digit:]]+)")))
             {
-                max_edges = std::stoi(token);
+                int arg = std::stoi(token);
+                args.push_back(arg); 
             }
-            else
+            else 
             {
                 std::stringstream ss; 
-                ss << "Non-numeric argument to -s flag\n\n";
+                ss << "Invalid non-numeric argument specified\n\n"; 
                 ss << "Help:\n\t./findProjection --help\n\n";
                 std::cerr << ss.str();
                 return -1;  
             }
         }
-        else
-        {
-            std::stringstream ss;
-            ss << "Unrecognized flag: " << option << "\n\n";
-            ss << "Help:\n\t./findProjection --help\n\n";
-            std::cerr << ss.str();
-            return -1;
+        for (int i = 0; i < options.size(); ++i)
+        { 
+            if (options[i] == "-s")
+            {
+                max_edges = args[i];
+            }
+            else if (options[i] == "-m")
+            {
+                max_step_iter = args[i];
+            }
+            else if (options[i] == "-p")
+            {
+                max_pull_iter = args[i];
+            }
+            else 
+            {
+                std::stringstream ss;
+                ss << "Unrecognized flag: " << options[i] << "\n\n";
+                ss << "Help:\n\t./findProjection --help\n\n";
+                std::cerr << ss.str();
+                return -1;
+            }
         }
-        infilename = argv[3];
-        outprefix = argv[4]; 
+        infilename = argv[argc - 2];
+        outprefix = argv[argc - 1]; 
     }
     else
     {
@@ -120,34 +173,19 @@ int main(int argc, char** argv)
     }; 
     BoundaryFinder* finder = new BoundaryFinder(
         area_tol, rng, infilename,
-        Polytopes::InequalityType::LessThanOrEqualTo,
-        func
+        Polytopes::InequalityType::LessThanOrEqualTo, func
     );
 
     // Compute boundary with assumption that region is simply connected
-    std::function<VectorXd(const Ref<const VectorXd>&, boost::random::mt19937&)>
-        mutate = [&coin_toss](const Ref<const VectorXd>& x, boost::random::mt19937& rng)
-    {
-        VectorXd mutated(x); 
-        const double delta = 0.1; 
-        for (unsigned i = 0; i < mutated.size(); ++i)
-        {
-            int toss = coin_toss(rng); 
-            if (!toss) mutated(i) += delta;
-            else       mutated(i) -= delta;
-        }
-        return mutated; 
-    };
+    double mutate_delta = 0.1 * getMaxDist<double>(finder->getVertices());
     std::function<bool(const Ref<const VectorXd>&)> filter = [](const Ref<const VectorXd>& x)
     {
         return false; 
     };
     const int n_init = 50; 
     MatrixXd init_input = finder->sampleInput(n_init);
-    const int min_step_iter = 4;
-    const int max_step_iter = 8; 
-    const int min_pull_iter = 10;
-    const int max_pull_iter = 20;
+    const int min_step_iter = 1;
+    const int min_pull_iter = 1;
     const unsigned sqp_max_iter = 100;
     const double sqp_tol = 1e-6;
     const double tau = 0.5; 
@@ -159,14 +197,13 @@ int main(int argc, char** argv)
     const double c1 = 1e-4; 
     const double c2 = 0.9; 
     const bool verbose = true;
-    const bool sqp_verbose = true;
+    const bool sqp_verbose = false;
     finder->run(
-        mutate, filter, init_input, min_step_iter, max_step_iter, min_pull_iter,
+        mutate_delta, filter, init_input, min_step_iter, max_step_iter, min_pull_iter,
         max_pull_iter, sqp_max_iter, sqp_tol, max_edges, tau, delta, beta,
         use_only_armijo, use_strong_wolfe, hessian_modify_max_iter, outprefix,
         c1, c2, verbose, sqp_verbose
     );
-    MatrixXd final_input = finder->getInput(); 
-
+    
     return 0;
 }
