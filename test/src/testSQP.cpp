6feb1e92a524bf62b9ce4996f458e0f4b7ff6a11
Kee Myoung Nam <kmnam@g.harvard.edu>
2022-10-19 16:41:54 +0900

    Added and passed Himmelblau's function test

--- test/src/testSQP.cpp
+++ test/src/testSQP.cpp
@@ -13,7 +13,7 @@
  *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
  *
  * **Last updated:**
- *     10/15/2022
+ *     10/19/2022
  */
 using namespace Eigen;
 double pi = 3.1415926539;
@@ -60,11 +60,19 @@ double Rosenbrock3D(const Ref<const VectorXd>& x)
     ); 
 }
 
+/**
+ * Himmelblau function.
+ */
+double Himmelblau(const Ref<const VectorXd>& x)
+{
+    return std::pow(x(0) * x(0) + x(1) - 11, 2) + std::pow(x(0) + x(1) * x(1) - 7, 2);
+}
+
+/**
+ * Run the default SQP optimizer on F1().
+ */
 BOOST_AUTO_TEST_CASE(TEST_SQP_F1)
 {
-    /**
-     * Run the default SQP optimizer on F1().
-     */
     using std::abs;
     const double delta = 1e-8; 
     const double beta = 1e-4;
@@ -108,11 +116,11 @@ BOOST_AUTO_TEST_CASE(TEST_SQP_F1)
     std::cout << "Passed: TEST_SQP_F1" << std::endl; 
 }
 
+/**
+ * Run the default SQP optimizer on F2().
+ */
 BOOST_AUTO_TEST_CASE(TEST_SQP_F2)
 {
-    /**
-     * Run the default SQP optimizer on F2().
-     */
     using std::abs;
     const double delta = 1e-8; 
     const double beta = 1e-4;
@@ -152,11 +160,11 @@ BOOST_AUTO_TEST_CASE(TEST_SQP_F2)
     delete opt;
 }
 
+/**
+ * Run the default SQP optimizer on F3().
+ */
 BOOST_AUTO_TEST_CASE(TEST_SQP_F3)
 {
-    /**
-     * Run the default SQP optimizer on F3().
-     */
     using std::abs;
     const double delta = 1e-8; 
     const double beta = 1e-4;
@@ -188,11 +196,11 @@ BOOST_AUTO_TEST_CASE(TEST_SQP_F3)
     delete opt;
 }
 
+/**
+ * Run the default SQP optimizer on the 2-D Rosenbrock function.
+ */
 BOOST_AUTO_TEST_CASE(TEST_SQP_ROSENBROCK2D)
 {
-    /**
-     * Run the default SQP optimizer on the 2-D Rosenbrock function.
-     */
     using std::abs;
     const double delta = 1e-8; 
     const double beta = 1e-4;
@@ -229,11 +237,11 @@ BOOST_AUTO_TEST_CASE(TEST_SQP_ROSENBROCK2D)
     delete opt;
 }
 
+/**
+ * Run the default SQP optimizer on the 3-D Rosenbrock function.
+ */
 BOOST_AUTO_TEST_CASE(TEST_SQP_ROSENBROCK3D)
 {
-    /**
-     * Run the default SQP optimizer on the 3-D Rosenbrock function.
-     */
     using std::abs;
     const double delta = 1e-8; 
     const double beta = 1e-4;
@@ -270,5 +278,47 @@ BOOST_AUTO_TEST_CASE(TEST_SQP_ROSENBROCK3D)
     BOOST_TEST(abs(solution(0) - 1.0) < tol);
     BOOST_TEST(abs(solution(1) - 1.0) < tol);
     BOOST_TEST(abs(solution(2) - 1.0) < tol);  
+    delete opt;
+}
+
+/**
+ * Run the default SQP optimizer on the Himmelblau function, with the input 
+ * constrained to a box containing the origin in the positive quadrant.
+ */
+BOOST_AUTO_TEST_CASE(TEST_SQP_HIMMELBLAU_POSQUADRANT)
+{
+    using std::abs;
+    const double delta = 1e-8; 
+    const double beta = 1e-4;
+    const double min_stepsize = 1e-8;
+    const int max_iter = 1000; 
+    const double tol = 1e-8;
+    const double x_tol = 1e-10;
+    const int hessian_modify_max_iter = 1000;
+    const double c1 = 1e-4;
+    const double c2 = 0.9;
+    const int line_search_max_iter = 10;
+    const int zoom_max_iter = 10;
+
+    MatrixXd A(4, 2);
+    A <<  1.0,  0.0,
+          0.0,  1.0,
+         -1.0,  0.0,
+          0.0, -1.0;
+    VectorXd b(4);
+    b << 0.0, 0.0, -10.0, -10.0;
+    SQPOptimizer<double>* opt = new SQPOptimizer<double>(2, 4, A, b);
+    VectorXd x(2); 
+    VectorXd l(4);
+    x << 5.0, 5.0;
+    l << 1.0, 1.0, 1.0, 1.0;
+    VectorXd solution = opt->run(
+        Himmelblau, x, l, delta, beta, min_stepsize, max_iter, tol, x_tol,
+        QuasiNewtonMethod::BFGS, RegularizationMethod::NOREG, 0,
+        hessian_modify_max_iter, c1, c2, line_search_max_iter,
+        zoom_max_iter, true, true, true
+    );
+    BOOST_TEST(abs(solution(0) - 3.0) < tol);
+    BOOST_TEST(abs(solution(1) - 2.0) < tol);
     delete opt;
 }
