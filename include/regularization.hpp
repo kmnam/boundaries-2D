dcd1253f2435198f0075a79bc3ec0701764fbfcd
Kee Myoung Nam <kmnam@g.harvard.edu>
2022-10-31 14:59:25 +0900

    First commit

---
+++ include/regularization.hpp
@@ -0,0 +1,21 @@
+/**
+ * Enum for L1 and L2 regularization.
+ *
+ * **Authors:**
+ *     Kee-Myoung Nam, Department of Systems Biology, Harvard Medical School
+ *
+ * **Last updated:**
+ *     10/31/2022
+ */
+
+#ifndef L1_L2_REGULARIZATION_HPP
+#define L1_L2_REGULARIZATION_HPP
+
+enum RegularizationMethod
+{
+    NOREG,
+    L1,
+    L2,
+};
+
+#endif
